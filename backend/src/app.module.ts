696a239de0babde8a963b213dcfd22051c2cd6ba
alanbarroncas <alanbarroncas@gmail.com>
2026-02-24 22:00:01 -0400

    feat: Adicionando módulos de projetos e tarefas com controladores, serviços e DTOs.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './core/prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Só para JWT depois
     PrismaModule,
     AuthModule,
+    UsersModule,
+    ProjectsModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
