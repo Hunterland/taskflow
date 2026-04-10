4f0c83ac214d8f200a3afa67ff9d796cd263f05b
alanbarroncas <alanbarroncas@gmail.com>
2026-04-10 02:22:51 -0400

    feat(components) criando componentes de login e home para configurar conexão com rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './features/auth/login/login';
+import { HomeComponent } from './features/dashboard/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
